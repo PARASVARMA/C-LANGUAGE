61d16b88fd514611cff8b970e47a03bb256da0bb
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-01 22:45:00 +0530

    Create input_scanf.c

---
+++ input_scanf.c
@@ -0,0 +1,11 @@
+//This is the scanf input function used to take the input by input function:-
+#include <stdio.h>
+
+int main() {
+  int a;
+  scanf("%d", &a);
+
+  printf("You entered: %d", a);
+
+  return 0;
+}
