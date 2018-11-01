c89f16c92f70507a928f1d09c46fdcfabab53527
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-01 22:47:00 +0530

    Create input_scanf_printf.c

---
+++ input_scanf_printf.c
@@ -0,0 +1,12 @@
+//How to take input by input function and take the output by output function:-
+//Scanf and printf function in C language:-
+#include <stdio.h>
+
+int main() {
+  int a;
+  scanf("%d", &a);
+
+  printf("You entered: %d", a);
+
+  return 0;
+}
