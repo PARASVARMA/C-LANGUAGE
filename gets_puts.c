fc413f3657f02732f32487277c6c26c7076c02ec
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-02 16:02:52 +0530

    Create gets_puts.c

---
+++ gets_puts.c
@@ -0,0 +1,14 @@
+//Gets and Puts function are used to take the input in c language:-
+
+#include <stdio.h>
+
+int main() {
+  char a[100];
+
+  gets(a); 
+
+  printf("You entered: ");
+  puts(a); 
+
+  return 0;
+} 
