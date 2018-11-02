f364e7649f14f59bf3e54ca976be6e861a600f06
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-02 16:01:22 +0530

    Create output.c

---
+++ output.c
@@ -0,0 +1,11 @@
+//This is how to take output from the program in c language:-
+#include <stdio.h>
+
+int main() {
+  char a = getchar();
+
+  printf("You entered: ");
+  putchar(a);
+
+  return 0;
+} 
