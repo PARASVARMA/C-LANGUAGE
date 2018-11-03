8ca82b517ef52b5ad896c093b7a97755202bd865
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-03 15:55:12 +0530

    Create singleline_comment.c

---
+++ singleline_comment.c
@@ -0,0 +1,11 @@
+//This example of single line comment:-
+#include <stdio.h>
+
+int main() {
+  int x = 42; //int for a whole number
+    
+  //%d is replaced by x
+  printf("%d", x);
+    
+  return 0;
+}
