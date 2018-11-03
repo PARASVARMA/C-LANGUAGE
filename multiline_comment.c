c11252442c2150ebcbdcf593fb523492a40e7150
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-03 15:53:22 +0530

    Create multiline_comment.c

---
+++ multiline_comment.c
@@ -0,0 +1,11 @@
+/*This example of multi line comment*/
+#include <stdio.h>
+
+/* A simple C program
+ *  Version 1.0
+ */
+int main() {
+  /* Output a string */
+  printf("Hello World!");
+  return 0;
+}
