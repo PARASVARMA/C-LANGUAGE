4faec5496a3576b2d4d4a92e5ac86d8ae807ae95
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-04 22:31:37 +0530

    Create multiply_operator.c

---
+++ multiply_operator.c
@@ -0,0 +1,13 @@
+This is how a multiply operator works specially multiply operator:-
+#include <stdio.h>
+
+int main() {
+  int length = 10;
+  int width = 5;
+  int area;
+
+  area = length * width;
+  printf("%d \n", area);  /* 50 */
+
+  return 0;
+}
