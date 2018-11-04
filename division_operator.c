89f50097a758744579f71a067c015ce9dd90fe9b
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-04 22:34:00 +0530

    Create division_operator.c

---
+++ division_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i1 = 10;
+  int i2 = 3;
+  int quotient, remainder;
+  float f1 = 4.2;
+  float f2 = 2.5;
+  float result;
+
+  quotient = i1 / i2; // 3
+  remainder = i1 % i2; // 1
+  result = f1 / f2; // 1.68
+
+  return 0;
+}
