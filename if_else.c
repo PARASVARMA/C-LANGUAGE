7c859bdbdb6a89ae3ae3078502d60e03089e301e
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-06 22:56:54 +0530

    Create if_else.c

---
+++ if_else.c
@@ -0,0 +1,13 @@
+//This the example of if else statement:-
+#include <stdio.h>
+
+int main() {
+  int score = 89;
+  
+  if (score >= 90)
+    printf("Top 10%%.\n");
+  else
+    printf("Less than 90.\n");
+    
+  return 0;
+}
