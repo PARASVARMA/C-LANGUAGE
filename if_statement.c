fa854654c00b4cd97400ada32cdbc15269573b92
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-06 22:52:42 +0530

    Create if_statement.c

---
+++ if_statement.c
@@ -0,0 +1,11 @@
+//This is the example of simple conditional statements:-
+#include <stdio.h>
+
+int main() {
+  int score = 89;
+  
+  if (score > 75)
+    printf("You passed.\n");
+    
+  return 0;
+}
