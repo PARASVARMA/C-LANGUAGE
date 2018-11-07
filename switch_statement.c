3c89fbb86cab070b87561a17c028293515172791
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-07 23:07:35 +0530

    Create switch_statement.c

---
+++ switch_statement.c
@@ -0,0 +1,21 @@
+//This example of Swithc statement:-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num = 3;
+
+  switch (num) {
+  case 1:
+    printf("One\n");
+    break;   
+  case 2:
+    printf("Two\n");
+    break;   
+  case 3:
+    printf("Three\n");
+    break;   
+  default:
+    printf("Not 1, 2, or 3.\n");
+}
+}
