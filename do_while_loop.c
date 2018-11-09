9faf59929b0c8788cc0c85ec4e971a5afabce92a
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-09 22:26:22 +0530

    Create do_while_loop.c

---
+++ do_while_loop.c
@@ -0,0 +1,13 @@
+//This example of Do while loop:-
+#include <stdio.h>
+
+int main() {
+  int count = 1;
+  
+  do {
+    printf("Count = %d\n", count);
+    count++;
+  } while (count < 8);
+    
+  return 0;
+}
