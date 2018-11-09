e4a3daea2d3400580805e1978f799e5a224877c8
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-09 22:24:59 +0530

    Create while_loop.c

---
+++ while_loop.c
@@ -0,0 +1,13 @@
+//this example of looping concept :-
+#include <stdio.h>
+
+int main() {
+  int count = 1;
+  
+  while (count < 8) {
+    printf("Count = %d\n", count);
+    count++;
+  }
+    
+  return 0;
+}
