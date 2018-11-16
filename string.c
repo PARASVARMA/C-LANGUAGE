85e67947e81d15ec45a02219cd5bfef6564be903
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-16 23:29:58 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,11 @@
+//This example of String:-
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char first_name[25];
+int age;
+printf("Enter your first name and age: \n");
+scanf("%s %d", first_name, &age); 
+getch();
+}
