4eb6bbf315442c589a94cdffde67408497c9f6f8
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-11-23 22:14:16 +0530

    Create structure_structure.c

---
+++ structure_structure.c
@@ -0,0 +1,16 @@
+//This example of structure with structure:-
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+typedef struct {
+  int x;
+  int y;
+} point;
+
+typedef struct {
+  float radius;
+  point center;
+} circle; 
+getch();
+}
