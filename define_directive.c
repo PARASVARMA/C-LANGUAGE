6c5751655993edfc74c38c6e089b0afdc84c037e
PARAS VERMA <40741495+PARASVARMA@users.noreply.github.com>
2018-12-11 19:34:35 +0530

    Create define_directive.c

---
+++ define_directive.c
@@ -0,0 +1,12 @@
+//This is how to use of define directive:-
+#include <stdio.h>
+#define PI 3.14
+#define AREA(r) (PI*r*r)
+
+int main() {
+  float radius = 2;
+  printf("%3.2f\n", PI);
+  printf("Area is %5.2f\n", AREA(radius));
+  printf("Area with radius + 1: %5.2f\n", AREA(radius+1));
+  return 0;
+} 
